885706f147849d124c96415e799ba85637e25521
BJP <batty211@gmail.com>
2026-06-02 17:05:07 +0700

    fix jwtwebtoken

--- src/app/api/auth/login/route.ts
+++ src/app/api/auth/login/route.ts
@@ -1,6 +1,5 @@
 import { prisma } from "@/lib/prisma";
 import bcrypt from "bcryptjs";
-import jwt from "jsonwebtoken";
 import { NextResponse } from "next/server";
 
 export async function POST(req: Request) {
@@ -13,49 +12,18 @@ export async function POST(req: Request) {
   });
 
   if (!user) {
-    return NextResponse.json(
-      { error: "Invalid username or password" },
-      { status: 401 }
-    );
+    return NextResponse.json({ error: "Invalid username or password" }, { status: 401 });
   }
 
-  const valid = await bcrypt.compare(
-    body.password,
-    user.passwordHash
-  );
+  const valid = await bcrypt.compare(body.password, user.passwordHash);
 
   if (!valid) {
-    return NextResponse.json(
-      { error: "Invalid username or password" },
-      { status: 401 }
-    );
+    return NextResponse.json({ error: "Invalid username or password" }, { status: 401 });
   }
-
-  const token = jwt.sign(
-    {
-      userId: user.id,
-      username: user.username,
-      role: user.role,
-    },
-    process.env.JWT_SECRET!,
-    {
-      expiresIn: "30d",
-    }
-  );
 
   const res = NextResponse.json({
     success: true,
   });
-
-  res.cookies.set(
-    "bjpkeep-token",
-    token,
-    {
-      httpOnly: true,
-      path: "/",
-      maxAge: 60 * 60 * 24 * 30,
-    }
-  );
 
   return res;
 }
